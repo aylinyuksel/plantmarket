483d774e0f9b2bb6a2c85201de9f06212c05aa0b
aylinyuksel <aylin.yuksel@istun.edu.tr>
2024-03-19 01:56:00 +0300

    8.videonun sonuna kadar.

--- shopifyHub/userauths/views.py
+++ shopifyHub/userauths/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.conf import settings
+
+
+
+User =settings.AUTH_USER_MODEL
 
 def register_view(request):
 
     if request.method == "POST":
-        form = UserRegisterForm()
-        print("User registered successfully")
+        form = UserRegisterForm(request.POST or None)
+        if form.is_valid():
+            new_user= form.save ()
+            username= form.cleaned_data.get("username")
+            messages.success(request,"Hey {username},Your account was created successfully." )
+            new_user = authenticate(username=form.cleaned_data['email'] ,password=form.cleaned_data['password1'])
+            
+            login(request,new_user)
+            return redirect("core:index")
+
     else:
-        print("User cannot be registered")
         form = UserRegisterForm()
     
 
@@ -15,3 +29,33 @@ def register_view(request):
         'form': form,
     }
     return render(request, "userauths/sign-up.html", context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request, f"You are already logged in")
+        return redirect("core:index")
+
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"User with {email} does not exist")
+
+        user = authenticate(request,email=email,password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are logged in.")
+            return redirect("core:index")
+        else:
+            messages.warning(request,"User  does not exist,create an account.")
+
+    context= {
+        
+
+    }
+
+    return render(request,"userauths/sign-in.html", context)
