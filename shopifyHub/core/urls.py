8d0f56f2f0cc4101c964fdc155fffa99de103a8a
aylinyuksel <aylin.yuksel@istun.edu.tr>
2024-05-09 07:06:59 +0300

    end of the carts

--- shopifyHub/core/urls.py
+++ shopifyHub/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.views import index, product_list_view, category_list_view, category_product_list_view, vendor_detail_view, vendor_list_view, product_detail_view, tag_list, ajax_add_review,search_view,filter_product,add_to_cart
+from core.views import index, product_list_view, category_list_view, category_product_list_view, vendor_detail_view, vendor_list_view, product_detail_view, tag_list, ajax_add_review, search_view, filter_product, add_to_cart, cart_view ,delete_item_from_cart, update_cart
 
 
 app_name ="core"
@@ -33,7 +33,18 @@ urlpatterns = [
     # filter
     path("filter-products/", filter_product, name="filter-product"),
 
-    # add to cart
+    # add to cart URL
     path("add-to-cart/", add_to_cart, name="add-to-cart"),
+
+     # Cart Page URL
+    path("cart/", cart_view, name="cart"),
+
+    # Delete item from cart 
+    path("delete-from-cart/", delete_item_from_cart, name="delete-from-cart"),
+
+    # Update item from cart 
+    path("update-cart/", update_cart, name="update-cart"),
+
+
     
 ]
