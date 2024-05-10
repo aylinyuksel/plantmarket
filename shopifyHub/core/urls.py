33c938242aa54dfe3ef3bc73b859dfcf11467efa
userayse404 <152299003+userayse404@users.noreply.github.com>
2024-05-10 23:04:20 +0300

    checkout page

--- shopifyHub/core/urls.py
+++ shopifyHub/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from core.views import index, product_list_view, category_list_view, category_product_list_view, vendor_detail_view, vendor_list_view, product_detail_view, tag_list, ajax_add_review, search_view, filter_product, add_to_cart, cart_view ,delete_item_from_cart, update_cart
+from core.views import index, product_list_view, category_list_view, category_product_list_view, vendor_detail_view, vendor_list_view, product_detail_view, tag_list, ajax_add_review, search_view, filter_product, add_to_cart, cart_view ,delete_item_from_cart, update_cart, checkout_view
 
 
 app_name ="core"
@@ -45,6 +45,8 @@ urlpatterns = [
     # Update item from cart 
     path("update-cart/", update_cart, name="update-cart"),
 
+    # checkout
+    path("checkout/", checkout_view, name="checkout"),
 
     
 ]
